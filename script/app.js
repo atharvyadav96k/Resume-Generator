c22738a36bd12069ef2cf04e546dc47c714cd3c2
atharvyadav96k <atharvyadav96k@gmail.com>
2024-04-30 19:13:51 +0530

    Feature You can add you own profile image

--- script/app.js
+++ script/app.js
@@ -1,4 +1,3 @@
-// Taking inputs from input box 
 const next = document.getElementById("next");
 const img_inp = document.getElementById('img-inp');
 const name_inp = document.getElementById("name-inp");
@@ -29,7 +28,39 @@ const PassingDate = Array.from(document.getElementsByClassName('PassingDate'));
 const LastYearGrades = Array.from(document.getElementsByClassName('LastYearGrades'));
 const Skills = Array.from(document.getElementsByClassName('Skills'));
 const MoreInformation = Array.from(document.getElementsByClassName('MoreInformation'));
+const profile_img = Array.from(document.getElementsByClassName('profile-img'));
 
 next.addEventListener('click', () => {
-    
-})
+    displayContent(user_name, name_inp.value);
+    displayContent(dob, dob_inp.value);
+    displayContent(Nationality, Nationality_inp.value)
+    displayContent(Email, email_inp.value)
+    displayContent(PersonalDetails, PersonalDetails_inp.value)
+    displayContent(Hobbies, Hobbies_inp.value)
+    displayContent(WorkingExperience, WorkingExperience_inp.value)
+    displayContent(Courses, Courses_inp.value)
+    displayContent(Courses, Courses_inp.value)
+    displayContent(PassingDate, Passingdate_inp.value)
+    displayContent(LastYearGrades, LastYearGrades_inp.value)
+    displayContent(Skills, Skills_inp.value)
+    // displayContent(MoreInformation, MoreAboutYourSelf_inp.value)
+
+    // Read the selected image file
+    const file = img_inp.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            // Set the source of each profile image to the data URL
+            profile_img.forEach((ele) => {
+                ele.src = e.target.result;
+            });
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
+function displayContent(obj, val){
+    obj.forEach((ele)=>{
+        ele.innerText = val;
+    })
+}
