6626b77041e310c7d2445b15f993a3b9769fd709
atharvyadav96k <atharvyadav96k@gmail.com>
2024-04-30 16:16:11 +0530

    selecting all input and display places

---
+++ script/app.js
@@ -0,0 +1,35 @@
+// Taking inputs from input box 
+const next = document.getElementById("next");
+const img_inp = document.getElementById('img-inp');
+const name_inp = document.getElementById("name-inp");
+const dob_inp = document.getElementById('dob-inp');
+const Nationality_inp = document.getElementById('Nationality-inp');
+const email_inp = document.getElementById('email-inp');
+const PersonalDetails_inp = document.getElementById('PersonalDetails-inp');
+const Hobbies_inp = document.getElementById('Hobbies-inp');
+const WorkingExperience_inp = document.getElementById('WorkingExperience-inp');
+const Courses_inp = document.getElementById('Courses-inp');
+const NameofInstitute_inp = document.getElementById('NameofInstitute-inp');
+const Passingdate_inp = document.getElementById('Passingdate-inp');
+const LastYearGrades_inp = document.getElementById('LastYearGrades-inp');
+const Skills_inp = document.getElementById('Skills-inp');
+const MoreAboutYourSelf_inp = document.getElementById('MoreAboutYourSelf-inp');
+
+// Place to display data
+const user_name = Array.from(document.getElementsByClassName('user-name'));
+const dob = Array.from(document.getElementsByClassName('dob'));
+const Nationality = Array.from(document.getElementsByClassName('Nationality'));
+const Email = Array.from(document.getElementsByClassName('Email'));
+const PersonalDetails = Array.from(document.getElementsByClassName('PersonalDetails'));
+const Hobbies = Array.from(document.getElementsByClassName('Hobbies'));
+const WorkingExperience = Array.from(document.getElementsByClassName('WorkingExperience'));
+const Courses = Array.from(document.getElementsByClassName('Courses'));
+const NameOfInstitute = Array.from(document.getElementsByClassName('NameOfInstitute'));
+const PassingDate = Array.from(document.getElementsByClassName('PassingDate'));
+const LastYearGrades = Array.from(document.getElementsByClassName('LastYearGrades'));
+const Skills = Array.from(document.getElementsByClassName('Skills'));
+const MoreInformation = Array.from(document.getElementsByClassName('MoreInformation'));
+
+next.addEventListener('click', () => {
+    
+})
